90329cc3e8deeacd0aa4b4c76bff34ae590d5ab3
MagneticShade <senya.krivorot11@gmail.com>
2023-06-08 22:47:36 +0200

    get requests - raw

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from  fastapi import FastAPI
+from models import User
+from uuid import uuid4,UUID
+from typing import List
 
 app=FastAPI()
+
+db:list[User]=[
+    User(
+        id="61a0149c-f27e-4824-85cc-28e1610ef6d1",
+        name="log1",
+        description="desc1"
+
+    ),
+
+    User(
+        id="83909f59-4924-459a-b749-c321aeed7a8e",
+        name="log2",
+        description="desc2"
+
+    )
+ ]
 
 @app.get("/")
 async def root():
     return {"hello":"world"}
+
+@app.get("/get/")
+async def getAllDb():
+    return db
+
+@app.get("/item/{id}")
+async def getItem(id: UUID):
+    for item in db:
+        if item.id==id:
+            return item
---
+++ models.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from uuid import UUID,uuid4
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id:Optional[UUID]=uuid4
+    name:str
+    description:str
